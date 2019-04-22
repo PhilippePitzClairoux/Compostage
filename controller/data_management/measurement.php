d1ca1584c81940762469bd159fcf021e9d86e6e5
Philippe Pitz Clairoux <philippepitz123@gmail.com>
2019-04-22 15:10:33 -0400

    changed a few constructors and started making code do handle the insertion of new measures

--- controller/data_management/measurement.php
+++ controller/data_management/measurement.php
@@ -1,18 +1,55 @@
 <?php
 
-    include_once("../ConnectionManager.php");
-    include_once("measurement_type.php");
+    include_once("measurement_value.php");
 
     class measurement {
 
         private $measurement_id;
-        private $measurement_type;
+        private $sensor_id;
         private $measurement_value;
         private $measurement_date;
+        private $measurement_types;
 
-        function __construct($measurement_id) {
 
-            $this->measurement_id = $measurement_id;
+        function __construct() {}
+
+        static function loadWithId($measurement_id) {
+
+            $instance = new self();
+            $instance->setMeasurementId($measurement_id);
+
+            $instance->fetch_data();
+            return $instance;
+        }
+
+        static function newMeasurement($sensor_id, $value, $type_id) {
+
+            $instance = new self();
+
+            $instance->setMeasurementValue($value);
+            $instance->setSensorId($sensor_id);
+            $instance->setMeasurementTypes($type_id);
+
+            return $instance;
+        }
+
+        public function getSensorId() {
+            return $this->sensor_id;
+        }
+
+        public function setSensorId($sensor_id): void {
+            if (!is_null($sensor_id))
+                $this->sensor_id = $sensor_id;
+        }
+
+
+        public function getMeasurementTypes() {
+            return $this->measurement_types;
+        }
+
+        public function setMeasurementTypes($measurement_types): void {
+            if (!is_null($measurement_types))
+                $this->measurement_types = $measurement_types;
         }
 
         public function getMeasurementId() {
@@ -22,15 +59,6 @@
         public function setMeasurementId($measurement_id) {
             if (!is_null($measurement_id))
                 $this->measurement_id = $measurement_id;
-        }
-
-        public function getMeasurementType() {
-            return $this->measurement_type;
-        }
-
-        public function setMeasurementType($measurement_type) {
-            if (!is_null($measurement_type))
-                $this->measurement_type = $measurement_type;
         }
 
         public function getMeasurementValue() {
@@ -52,5 +80,77 @@
             if (!is_null($measurement_date))
                 $this->measurement_date = $measurement_date;
         }
-        
+
+        function fetch_data() {
+
+            $conn = getConnection();
+            $statement = $conn->prepare("SELECT * FROM measures WHERE measure_id = ?");
+
+            $statement->bind_param("i", $this->measurement_id);
+
+            if (!$statement->execute()) {
+                mysqli_close($conn);
+                die("Cannot fetch measure data");
+            }
+
+            $result = $statement->get_result();
+
+            while($row = $result->fetch_assoc()) {
+
+                $this->measurement_date = $row["measure_timestamp"];
+                $this->sensor_id = $row["sensor_id"];
+            }
+
+            mysqli_free_result($result);
+
+
+            $statement = $conn->prepare("SELECT * FROM ta_measure_type WHERE measure_id = ?");
+            $statement->bind_param("i", $this->measurement_id);
+
+            if (!$statement->execute()) {
+                mysqli_close($conn);
+                die("Cannot fetch types...");
+            }
+
+            $this->measurement_types = array();
+            $this->measurement_value = array();
+
+            $result = $statement->get_result();
+
+            while($row = $result->fetch_assoc()) {
+
+                array_push($this->measurement_types, $row["measure_type_id"]);
+            }
+
+            mysqli_free_result($result);
+
+            foreach($this->measurement_types as $type_id) {
+
+                $index = array_push($this->measurement_value,new measurement_value($this->measurement_id, $type_id)) -1;
+                ($this->measurement_value[$index])->fetch_data();
+            }
+
+        }
+
+        //TODO : insert the new data in the database
+        function insert_data() {
+
+            $conn = getConnection();
+            $statement = $conn->prepare("INSERT INTO measures(sensor_id, measure_timestamp) VALUES (?, ?) ");
+
+            date_default_timezone_set('America/New_York');
+            $date = date("r");
+
+            $statement->bind_param("is", $this->sensor_id, $date);
+
+            if (!$statement->execute()) {
+                mysqli_close($conn);
+                die("Cannot insert data inside measures...");
+            }
+
+        }
+
     }
+
+    $tmp = measurement::newMeasurement(1, 10, 1);
+    $tmp->insert_data();
