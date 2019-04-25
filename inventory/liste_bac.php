1942b12d88b04a06d9ea55b60ac97f26792c48c3
DESKTOP-OHJLOHD\TheTh <thethousandbeanster@hotmail.com>
2019-04-25 12:26:05 -0400

    Liste bac existe

---
+++ inventory/liste_bac.php
@@ -0,0 +1,77 @@
+
+<!--********************************
+    Fichier : liste_bac.php
+    Auteur : Benoit Audettr-Chavigny
+    Fonctionnalité : FB
+    Date : 2019-04-25
+
+    Vérification :
+    Date                Nom                 Approuvé
+    ====================================================
+
+    Historique de modifications :
+    Date                Nom                 Description
+    ======================================================
+
+ ********************************/-->
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Liste Bac</title>
+  </head>
+  <body>
+
+
+    <table class="col-m-6 left">
+
+      <thead>
+        <tr>
+          <th>
+            Nom Bac
+          </th>
+          <th>
+            ID
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td colspan="2">
+            <ul class="left">
+              <li>
+                example
+              </li>
+              <li>
+                example
+              </li>
+              <li>
+                example
+              </li>
+              <li>
+                example
+              </li>
+              <li>
+                example
+              </li>
+              <li>
+                example
+              </li>
+              <li>
+                example
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+
+    </table>
+
+    <button class="left">Ajouter</button><br />
+    <button class="left">Modifier</button><br />
+    <button class="left">Supprimer</button><br />
+    <button class="left">Ok</button><br />
+
+
+  </body>
+</html>
