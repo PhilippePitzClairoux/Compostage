c99f5fc42f68955d946bf2901396a5684fe43c26
DESKTOP-OHJLOHD\TheTh <thethousandbeanster@hotmail.com>
2019-04-26 15:06:03 -0400

    Basic shell outlines

---
+++ inventory/ajouter_bac.php
@@ -0,0 +1,33 @@
+
+<!--********************************
+    Fichier : ajouter_bac.php
+    Auteur : Benoit Audettr-Chavigny
+    Fonctionnalité : FB
+    Date : 2019-04-26
+
+    Vérification :
+    Date                Nom                 Approuvé
+    ====================================================
+
+    Historique de modifications :
+    Date                Nom                 Description
+    ======================================================
+
+ ********************************/-->
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Ajouter/modifier Bac</title>
+  </head>
+  <body>
+
+    <form method="post">
+      <label for="nom">Nom Bac: </label> <input type="text" name="nom" id="nom" required /><br />
+      <button type="button" onclick="location.href='liste_bac.php'">Cancel</button>
+      <button type="submit">Ajouter</button>
+    </form>
+
+
+  </body>
+</html>
