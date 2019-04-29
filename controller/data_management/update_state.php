19efdecdcbcbc40daba7de90e43f9f19ce6a5f75
x <philippepitz123@gmail.com>
2019-04-29 15:45:51 +0000

    AYYYYYYYYYY

---
+++ controller/data_management/update_state.php
@@ -0,0 +1,74 @@
+<?php
+
+    include_once("../ConnectionManager.php");
+
+    class update_state {
+
+        private $update_state_id;
+        private $update_state;
+        private $update_state_description;
+
+        private function __construct() {}
+
+        public function loadWithId($update_state_id) {
+            $instance = new self();
+
+            $instance->setUpdateStateId($update_state_id);
+            $instance->fetch_data();
+
+            return $instance;
+        }
+
+        public function getUpdateStateId() {
+            return $this->update_state_id;
+        }
+
+        public function setUpdateStateId($update_state_id): void {
+            $this->update_state_id = $update_state_id;
+        }
+
+        public function getUpdateState() {
+            return $this->update_state;
+        }
+
+        public function setUpdateState($update_state): void {
+            $this->update_state = $update_state;
+        }
+
+        public function getUpdateStateDescription() {
+            return $this->update_state_description;
+        }
+
+        public function setUpdateStateDescription($update_state_description): void {
+            $this->update_state_description = $update_state_description;
+        }
+
+        public function fetch_data() {
+
+            $conn = getConnection();
+            $statement = $conn->prepare("SELECT * FROM update_state WHERE update_state_id = ?");
+            $statement->bind_param("i", $this->update_state_id);
+
+            if (!$statement->execute()) {
+                mysqli_close($conn);
+                throw new Exception($statement->error);
+            }
+
+            $result = $statement->get_result();
+
+            if ($result->num_rows === 0) {
+                mysqli_free_result($result);
+                mysqli_close($conn);
+                throw new Exception("Update state does not exist.");
+            }
+
+            while ($row = $result->fetch_assoc()) {
+
+                $this->setUpdateState($row["update_state"]);
+                $this->setUpdateStateDescription($row["update_state_description"]);
+            }
+
+            mysqli_free_result($result);
+            mysqli_close($conn);
+        }
+    }
