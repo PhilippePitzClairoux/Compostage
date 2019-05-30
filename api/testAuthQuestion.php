f0f0f9cd133d5a51c579ef1e2a9ad0c5a053a6b9
x <philippepitz123@gmail.com>
2019-05-30 11:37:53 -0400

    finished up the login and the password recovery

--- api/testAuthQuestion.php
+++ api/testAuthQuestion.php
@@ -4,10 +4,10 @@ include_once($_SERVER["DOCUMENT_ROOT"] . "/controller/SessionUtils.php");
 include_once($_SERVER["DOCUMENT_ROOT"] . "/controller/data_management/user.php");
 include_once($_SERVER["DOCUMENT_ROOT"] . "/controller/SecurityUtils.php");
 
-if (!empty($_POST["username"]) AND !empty($_POST["answer"])) {
+if (!empty($_GET["username"]) AND !empty($_GET["answer"])) {
 
-    $user = user::loadWithId(sanitize_input($_POST["username"]));
-    if (password_verify(sanitize_input($_POST["answer"]), $user->getUserAuthAnswer())) {
+    $user = user::loadWithId(sanitize_input($_GET["username"]));
+    if (password_verify(sanitize_input($_GET["answer"]), $user->getUserAuthAnswer())) {
 
         create_session();
 
--- js/login_validation.js
+++ js/login_validation.js
@@ -4,20 +4,26 @@ function validate() {
     let pass1 = document.getElementById("inputPasswordOne");
     let pass2 = document.getElementById("inputPasswordTwo");
 
+    if (pass1.value !== "" && pass2.value !== "") {
 
-    if ((!password_is_valid(pass1.value) || !password_is_valid(pass2.value)) || document.getElementById("error").innerText !== "") {
-        console.log('invalid pass');
-        document.getElementById("error").innerText = "Invalid password (needs to be 5 characters or more)";
-        return false;
+        if (!password_is_valid(pass1.value)) {
+            document.getElementById("error").innerText = "Invalid password (needs to be 5 characters or more)";
+            document.getElementById('submit').disabled = true;
+            return false;
+        }
+
+        if (pass1.value !== pass2.value) {
+            document.getElementById("error").innerText = "Passwords do not match";
+            document.getElementById('submit').disabled = true;
+            return false;
+        }
+
+        document.getElementById('error').innerText = "";
+        document.getElementById('submit').disabled = false;
+        return true;
     }
 
-    if (pass1.value !== pass2.value) {
-        console.log('dont match');
-        document.getElementById("error").innerText = "Make sure you typed the new password twice.";
-        return false;
-    }
-
-    return true;
+    return false;
 }
 
 function password_is_valid(value) {
@@ -27,75 +33,79 @@ function password_is_valid(value) {
 function lock_form(toLock) {
 
     for (let i = 0; i < toLock.length; i++)
-        toLock[i].style.disabled = true;
+        toLock[i].disabled = true;
 }
 
 
 function unlock_form(toLock) {
 
     for (let i = 0; i < toLock.length; i++)
-        toLock[i].style.disabled = false;
+        toLock[i].disabled = false;
 }
 
 window.onload = function() {
 
     let password = [document.getElementById("inputPasswordOne"), document.getElementById("inputPasswordTwo")];
-    let form = document.getElementById("form_xd");
+    let form = new Array();
     let error = document.getElementById("error");
     let question = document.getElementById('question');
     let answer = document.getElementById("inputAnswer");
-    let user = Cookies.get('user');
+    let user = JSON.parse(Cookies.get('user'));
+
+    form.push(password[0]);
+    form.push(password[1]);
+    form.push(document.getElementById('submit'));
+
+    console.log(user);
+    lock_form(form);
 
     if (user) {
 
-        console.log(user);
-        console.log(password);
-
-        question.innerText = Cookies.get('user')["user_auth_question"];
+        question.innerText = user["user_auth_question"];
 
 
         for (let i = 0; i < password.length; i++) {
 
             let pass = password[i];
 
-            pass.addEventListener('blur', function () {
-                if (!password_is_valid(pass.value)) {
-                    error.innerText = "Invalid password (needs to be 5 characters or more)";
-                    return false;
-                } else {
-                    error.innerText = "";
-                    return true;
-                }
+            pass.addEventListener('keyup', function () {
+                validate();
             });
         }
 
         answer.addEventListener("keyup", function() {
 
-            let xhr = new XMLHttpRequest();
-            let query = "http://localhost:8080/api/testAuthQuestion.php?username=" + user["username"] + "&answer=" + answer.value;
+            if (answer.value !== "") {
 
-            console.log(query);
+                let xhr = new XMLHttpRequest();
+                let query = "http://localhost:8080/api/testAuthQuestion.php?username=" + user["username"] + "&answer=" + answer.value;
 
-            xhr.open('GET', query);
-            xhr.responseType = 'json';
 
-            xhr.send();
+                xhr.open('GET', query);
+                xhr.responseType = 'json';
+                xhr.send();
 
-            xhr.onload = function() {
-                if (xhr.status !== 200)
-                    console.log("This is not supposed to happen...");
-                else {
+                xhr.onload = function () {
 
-                    if (xhr.response["error"]) {
-                        document.location.href = "http://localhost:8080/login.html";
-                    } else if (xhr.response["status"] !== "completed") {
-                        error.innerText = "Invalid answer";
-                    } else {
+                    console.log(xhr.response);
 
+                    if (xhr.status !== 200)
+                        console.log("This is not supposed to happen...");
+                    else {
+                        if (xhr.response["error"]) {
+                            document.location.href = "http://localhost:8080/login.html";
+                        } else if (xhr.response["status"] !== "completed") {
+                            error.innerText = "Invalid answer";
+                            lock_form(form);
+                        } else {
+                            document.getElementById('inputAnswer').disabled = true;
+                            error.innerText = "";
+                            unlock_form(password);
+                            Cookies.remove('user');
+                        }
                     }
-
-                }
-            };
+                };
+            }
 
         });
 
